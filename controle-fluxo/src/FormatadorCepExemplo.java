938544a9bfd947649c426dc0614ec9da93317c4e
HendriuNepuz <andreinepo@gmail.com>
2024-05-29 21:43:05 -0300

    Aula Java Básci oexercícios

---
+++ controle-fluxo/src/FormatadorCepExemplo.java
@@ -0,0 +1,18 @@
+public class FormatadorCepExemplo {
+    public static void main(String[] args) {
+        try {
+            String cepFormatado = formatarCep("70673205");
+            System.out.println(cepFormatado);
+        } catch (CepInvalidoException e) {
+            // TODO Auto-generated catch block
+            System.out.println("CEP não corresponde as regras de negócio.");;
+        }
+    }
+    static String formatarCep(String cep) throws CepInvalidoException{
+        if(cep.length() != 8)
+          throw new CepInvalidoException();
+        
+          //simulando um cep formatado
+          return cep;
+    }
+}
