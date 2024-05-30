304dccd7c2ef9e7ccfd1e60dbe2b3aba2809556b
HendriuNepuz <andreinepo@gmail.com>
2024-05-29 21:42:02 -0300

    Exercícios da aula da Java Básico

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,79 @@
+public class Operadores {
+    public static void main(String[] args) {
+        boolean condicao1=true;
+
+        boolean condicao2=true;
+        if (condicao1 && (7 > 4)) {
+            System.out.println("As duas condições são verdadeiras.");
+                       
+        }
+        if (condicao1 || condicao2) {
+            System.out.println("Uma das condições é verdadeira!");
+        }
+        System.out.println("Fim!");
+
+        
+        /*String nomeUm = "Andrei";
+        String nomeDois = new String("Andrei");
+
+        System.out.println(nomeUm.equals(nomeDois));       
+        int numero1 = 1;
+        int numero2 = 2;
+
+        boolean simNao = numero1 == numero2;
+
+        if(numero1 == numero2){
+        System.out.println("A nossa condição é verdadeira");
+        }
+        simNao = numero1 != numero2;
+        System.out.println("numeroUm é diferente ao numeroDois? " + simNao);
+
+        simNao = numero1 > numero2;
+        System.out.println("numeroUm é maior ao numeroDois? " + simNao);
+
+        simNao = numero1 < numero2;
+        System.out.println("numeroUm é menor ao numeroDois? " + simNao);*/
+     
+        /* boolean variavel = true;
+      System.out.println(!variavel);
+      System.out.println(variavel);
+
+      variavel = !variavel;
+      System.out.println(variavel); */
+
+        /*   int numero = 5;
+        //x repetição
+        numero++; //mesma representação de numero = numero + 1
+        System.out.println(numero--); //imprime o numero e depois incrementa
+        System.out.println(++ numero);
+        System.out.println(numero); */
+     /*    int numero = +5;
+
+        numero = -numero;
+        System.out.println(numero);
+        
+        numero = -numero;
+        System.out.println(numero); */
+
+     /*   String nomeCompleto = "LINGUAGEM" + "JAVA";
+        System.out.println(nomeCompleto);
+		
+        String concatenacao ="?"; 
+
+        concatenacao = 1+1+1+"1";
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+1;
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+"1";
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+1+1+1;
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+(1+1+1);
+        System.out.println(concatenacao);*/
+    }
+
+}
--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,22 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //tipos primitivos
+        //classe String que representa texto na aplicação
+        String meuNome = "Andrei Nepomuceno";
+        System.out.println(meuNome);
+        double salarioMinimo = 2500.50;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 1;
+        numero = 2;
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
+
+        
+        
     }
 }
